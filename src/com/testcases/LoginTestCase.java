2831814a8e440d6bed35c18380e236ab18a14ab6
Goricipati <GANITHA3@ford.com>
2022-10-26 21:06:30 -0400

    commiting the changes

---
+++ src/com/testcases/LoginTestCase.java
@@ -0,0 +1,22 @@
+package com.testcases;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+public class LoginTestCase {
+
+	public static RemoteWebDriver driver;
+	
+	
+
+	@Test
+	
+	public void LoginTest()
+	
+	
+	{
+		
+		String url = "http://19.51.3.188:4444/wd/hub";
+	
+		
+}
+}
